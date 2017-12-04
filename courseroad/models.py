de0849c41ac5c64470c3fe32368fd4d38bb77378
Juan Diego Fajardo <jfajardo@mit.edu>
2017-12-03 21:07:11 -0500

    When create user, automatically create years and semesters in their road.

--- courseroad/models.py
+++ courseroad/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from rest_framework import validators
 
 # Create your models here.
@@ -47,3 +50,45 @@ class Year(models.Model):
 
     class Meta:
         ordering = ('year_id',)
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#
+#     @receiver(post_save, sender=User)
+#     def create_user_profile(sender, instance, created, **kwargs):
+#         if created:
+#             num_years = 4
+#             num_semesters = 3
+#
+#             year_titles = ['Freshman' 'Sophomore', 'Junior', 'Senior']
+#             semester_types = ['fall', 'iap', 'spring']
+#
+#             for i in range(num_years):
+#                 year = Year.objects.create(user=instance, year_id=i, title=year_titles[i])
+#                 for j in range(num_semesters):
+#                     semester = Semester.objects.create(year=year, semester_id=j, type=semester_types[j])
+
+            # Profile.objects.create(user=instance)
+
+    # @receiver(post_save, sender=User)
+    # def save_user_profile(sender, instance, **kwargs):
+    #     instance.profile.save()
+
+
+class Profile(User):
+    class Meta:
+        proxy = True
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            num_years = 4
+            num_semesters = 3
+
+            year_titles = ['Freshman', 'Sophomore', 'Junior', 'Senior']
+            semester_types = ['fall', 'iap', 'spring']
+
+            for i in range(num_years):
+                year = Year.objects.create(user=instance, year_id=i, title=year_titles[i])
+                for j in range(num_semesters):
+                    semester = Semester.objects.create(year=year, semester_id=j, type=semester_types[j])
