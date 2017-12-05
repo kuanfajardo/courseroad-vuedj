647c66d8459bacb5cae55794c9b5a1545f23960e
Juan Diego Fajardo <jfajardo@mit.edu>
2017-12-05 08:12:40 -0500

    Clean up Requirement model.

--- courseroad/engine.py
+++ courseroad/engine.py
@@ -2,161 +2,6 @@ import json
 # from courseroad.models import Subject
 
 
-path_keys = {}
-real_sat = {}
-real_path_sat = {}
-
-
-def tagged(subject: str, tag: str):
-    obj = json.load(open("tags.json"))
-
-    if tag not in obj:
-        return False
-
-    return subject in obj[tag]
-
-def check(req_obj, subjects):
-    ct = req_obj["count"]
-    required = ct == -1
-
-    if required:
-        for subject in req_obj["reqs"]:
-            sat_val = subject in subjects
-
-            update_real_sat(subject,
-                            (sat_val, [subject] if sat_val else []),
-                            req_obj["path"] if "path" in req_obj else None)
-
-        return
-
-    tag_req = "tag" in req_obj
-
-    co = 0
-    sat = []
-    for cl in subjects:
-        if tag_req and tagged(cl, req_obj["tag"]) or not tag_req and cl in req_obj["reqs"]:
-            co += 1
-            sat += [cl]
-
-        if co == ct:
-            break
-
-    update_real_sat(req_obj["idd"], (co >= ct, sat), req_obj["path"] if "path" in req_obj else None)
-
-def check_requirements(req_obj, subjects, path=None):
-    typ = req_obj['type']
-
-    if is_leaf(req_obj):
-        if path is not None:
-            req_obj.update({'path': path})
-
-            if path[0] not in real_path_sat:
-                real_path_sat[path[0]] = {}
-
-            if path[0] not in path_keys:
-                path_keys[path[0]] = {}
-
-            if path[1] in path_keys[path[0]]:
-                path_keys[path[0]][path[1]].add(req_obj["idd"])
-            else:
-                path_keys[path[0]][path[1]] = {req_obj["idd"]}
-
-        check(req_obj, subjects)
-        return
-
-    if typ == 'path':
-        for i, path in enumerate(req_obj['paths']):
-            check_requirements(path, subjects, (req_obj["idd"], path["pid"]))
-        return
-
-    for req in req_obj['reqs']:
-        check_requirements(req, subjects, path)
-
-def update_real_sat(key, val, path=None):
-    if path is None:
-        real_sat.update({key: val})
-        return
-
-    real_sat.update({key: val})
-
-    if path[1] in real_path_sat[path[0]]:
-        real_path_sat[path[0]][path[1]][key] = val
-    else:
-        real_path_sat[path[0]][path[1]] = {key: val}
-
-def is_leaf(obj):
-    return obj["type"] == "leaf"
-
-def is_path(obj):
-    return obj["type"] == "path"
-
-def is_sub_req(obj):
-    return obj["type"] == "req"
-
-# TODO: fix bad algorithm - if completed branch has <= # of items, wont get notices
-def evaluate_path_sat(path_id):
-    best_path = -1
-    max_ct = -1
-
-    for path, reqs in real_path_sat[path_id].items():
-        ct = 0
-        for req, val in reqs.items():
-            req_sat = val[0]
-            if req_sat:
-                ct += 1
-
-        if ct > max_ct:
-            best_path = path
-            max_ct = ct
-
-    return best_path, max_ct
-
-def evaluate_paths():
-    all_paths = {}
-    for path_id in path_keys:
-        best_path, max_ct = evaluate_path_sat(path_id)
-        all_paths[path_id] = (best_path, max_ct)
-
-    return all_paths
-
-def complete_sat(obj):
-    typ = obj["type"]
-
-    if typ == "leaf":
-        if obj["count"] != -1:
-            return
-
-        req_sat = True
-        sat_subjects = []
-        for subject in obj["reqs"]:
-            temp_req_sat, temp_sat_subjects = real_sat[subject]
-
-            req_sat &= temp_req_sat
-            sat_subjects += temp_sat_subjects
-
-        real_sat[obj["idd"]] = (req_sat, sat_subjects)
-
-        return
-
-    if typ == "path":
-        for path in obj["paths"]:
-            complete_sat(path)
-
-        return
-
-    if typ == "req":
-        req_sat = True
-        sat_subjects = []
-        for req in obj["reqs"]:
-            complete_sat(req)
-
-            temp_req_sat, temp_sat_subjects = real_sat[req["idd"]]
-
-            req_sat &= temp_req_sat
-            sat_subjects += temp_sat_subjects
-
-        real_sat[obj["idd"]] = (req_sat, sat_subjects)
-
 def prepare_output(obj, sat, level=0):
     typ = obj["type"]
     name = obj["name"] if "name" in obj else None
@@ -226,83 +71,26 @@ def prepare_output(obj, sat, level=0):
 
 
 
-class RRequirement:
-    def __init__(self, req_file):
-        if (type(req_file) is str):
-            req_file = open(req_file)
-
-        self.req_obj = json.load(req_file)
-        self.path_keys = self.get_path_keys(self.req_obj)
-
-        self.type = self.req_obj["type"]
-        self.req_id = self.req_obj["idd"]
-        self.count = self.req_obj["count"]
-
-        self.name = self.req_obj["name"] if "name" in self.req_obj else ""
-
-        if self.type == "path":
-            self.paths = self.req_obj["paths"]
-
-        if self.type == "req":
-            self.requirements = self.req_obj["reqs"]
-
-        if self.type == "leaf":
-            if "reqs" in self.req_obj:
-                self.requirements = self.req_obj["reqs"]
-
-            if "tag" in self.req_obj:
-                self.tag = self.req_obj["tag"]
-
-        if "pid" in self.req_obj:
-            self.path_id = self.req_obj["pid"]
-
-
-
-
+class RequirementFactory:
     @classmethod
-    def get_path_keys(self, req_obj, path=None, path_keys=None):
-        """
-        Recursively extract all path keys
-        :param req_obj:
-        :param path:
-        :param path_keys:
-        :return:
-        """
+    def create(cls, req_obj: object):
+        if type(req_obj) is str:
+            file_obj = open(req_obj)
+            req_obj = json.load(file_obj)
 
-        if path_keys is None:
-            path_keys = {}
+        typ = req_obj["type"]
 
-        if is_leaf(req_obj):
-            if path is not None:
-                path_req_id, path_id = path
+        if typ == "leaf":
+            if "reqs" in req_obj:
+                return LeafRequirement(req_obj)
+            if "tag" in req_obj:
+                return TagRequirement(req_obj)
 
-                if path_req_id not in path_keys:
-                    path_keys[path_req_id] = {}
+        if typ == "path":
+            return PathRequirement(req_obj)
 
-                if path_id in path_keys[path_req_id]:
-                    path_keys[path_req_id][path_id].add(req_obj["idd"])
-                else:
-                    path_keys[path_req_id][path_id] = {req_obj["idd"]}
-
-        elif is_path(req_obj):
-            req_id = req_obj["idd"]
-
-            for path_obj in req_obj['paths']:
-                path_id = path_obj["pid"]
-                self.get_path_keys(path_obj, (req_id, path_id), path_keys)
-
-        else:
-            for req in req_obj['reqs']:
-                self.get_path_keys(req, path, path_keys)
-
-        return path_keys
-
-    def check_requirements(self, subjects):
-        """
-
-        :param subjects:
-        :return:
-        """
+        if typ == "req":
+            return ListRequirement(req_obj)
 
 
 class BaseRequirement:
@@ -335,30 +123,7 @@ class BaseRequirement:
         pass
 
 
-
-class ReqFactory:
-    @classmethod
-    def create(cls, req_obj: object):
-        if type(req_obj) is str:
-            file_obj = open(req_obj)
-            req_obj = json.load(file_obj)
-
-        typ = req_obj["type"]
-
-        if typ == "leaf":
-            if "reqs" in req_obj:
-                return LeafRequirement(req_obj)
-            if "tag" in req_obj:
-                return TagRequirement(req_obj)
-
-        if typ == "path":
-            return Path(req_obj)
-
-        if typ == "req":
-            return Requirement(req_obj)
-
-
-class Requirement(BaseRequirement): # type == "req"
+class ListRequirement(BaseRequirement): # type == "req"
     def __init__(self, req_obj):
         super().__init__(req_obj)
 
@@ -366,7 +131,7 @@ class Requirement(BaseRequirement): # type == "req"
         self.requirements = []
 
         for req in req_obj["reqs"]:
-            self.requirements.append(ReqFactory.create(req))
+            self.requirements.append(RequirementFactory.create(req))
 
     def is_satisfied(self, subjects:set, sat_dict:dict=None):
         if sat_dict is None:
@@ -392,7 +157,7 @@ class Requirement(BaseRequirement): # type == "req"
         return (sat, sat_dict)
 
 
-class Path(BaseRequirement): # type == "path"
+class PathRequirement(BaseRequirement): # type == "path"
     def __init__(self, req_obj):
         super().__init__(req_obj)
 
@@ -400,7 +165,7 @@ class Path(BaseRequirement): # type == "path"
         self.paths = []
 
         for path in req_obj["paths"]:
-            self.paths.append(ReqFactory.create(path))
+            self.paths.append(RequirementFactory.create(path))
 
     def is_satisfied(self, subjects:set, sat_dict:dict=None):
         if sat_dict is None:
@@ -421,12 +186,7 @@ class Path(BaseRequirement): # type == "path"
         return (sat, sat_dict)
 
 
-class Leaf(BaseRequirement): # type == "leaf"
-    def __init__(self, req_obj):
-        super().__init__(req_obj)
-
-
-class TagRequirement(Leaf): # type == "leaf", has TAG
+class TagRequirement(BaseRequirement): # type == "leaf", has TAG
     def __init__(self, req_obj):
         super().__init__(req_obj)
         self.tag = req_obj["tag"]
@@ -448,7 +208,7 @@ class TagRequirement(Leaf): # type == "leaf", has TAG
         return (sat, sat_dict)
 
 
-class LeafRequirement(Leaf): # type == "leaf", has REQS
+class LeafRequirement(BaseRequirement): # type == "leaf", has REQS
     def __init__(self, req_obj):
         super().__init__(req_obj)
         self.requirements = req_obj["reqs"]
@@ -493,10 +253,8 @@ def run(subjects: set, req_file):
 
 
 req_file = "./static/courseroad/6-3.req"
-# r = Requirement(req_file)
-r = ReqFactory.create(req_file)
+r = RequirementFactory.create(req_file)
 print(r.is_satisfied({"6.009", "6.004", "6.006", "6.01", "6.0001"}))
-
 
 
 class Road:
